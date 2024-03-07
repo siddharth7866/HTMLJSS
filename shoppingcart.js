5564bdf918bb0df988bdeb31dd87df776b95a42d
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2024-03-07 12:09:04 +0530

    files added

---
+++ shoppingcart.js
@@ -0,0 +1,66 @@
+let sid=document.getElementsByTagName('h1')[0]
+
+function Sid() {
+    // sid.innerHTML="I am clicked and now I am Sid Vishen"
+    // document.write(5+5)
+    // window.alert("Siddharth")
+    document.getElementsByTagName('button')[0].style.display="none"
+
+    window.print()
+
+}
+
+
+sid.addEventListener('click',Sid)
+
+
+// let x,y,z
+
+// x="sodd"
+
+// console.log(x)
+
+//this sis comment and it will not be executed
+
+ x="siddharth"
+// let x=10
+
+console.log(x)
+
+let A1="Siddharth"
+let A2="vishen"
+A1="vishen"
+console.log(A1)
+
+const _Name="Siddharth hidden"
+//_Name is a hidden variable
+
+console.log(_Name)
+
+let age=12,name="naamsiddharth"
+console.log(name)
+
+
+let siddate=new Date()
+console.log(siddate.setFullYear(2024))
+
+
+let rand=Math.random()
+console.log(rand)
+
+// let _1="Siddharth"
+// console.log(_1)
+
+let Name="siddharth",Age=23,Prof="Web Developer"
+
+console.log(Name)
+console.log(Age)
+console.log(Prof)
+
+let numb="5"
+console.log(numb+5+9+10)
+
+let x
+
+x=10
+console.log(x)

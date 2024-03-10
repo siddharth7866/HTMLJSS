0399bb7ce891359c5f6d24b2b4fdc5290e9502a6
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2024-03-10 15:15:20 +0530

    code added

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,66 +1,137 @@
-let sid=document.getElementsByTagName('h1')[0]
+// // let sid=document.getElementsByTagName('h1')[0]
 
-function Sid() {
-    // sid.innerHTML="I am clicked and now I am Sid Vishen"
-    // document.write(5+5)
-    // window.alert("Siddharth")
-    document.getElementsByTagName('button')[0].style.display="none"
+// // function Sid() {
+// //     // sid.innerHTML="I am clicked and now I am Sid Vishen"
+// //     // document.write(5+5)
+// //     // window.alert("Siddharth")
+// //     document.getElementsByTagName('button')[0].style.display="none"
 
-    window.print()
+// //     window.print()
 
-}
-
-
-sid.addEventListener('click',Sid)
+// // }
 
 
-// let x,y,z
+// // sid.addEventListener('click',Sid)
 
-// x="sodd"
 
+// // // let x,y,z
+
+// // // x="sodd"
+
+// // // console.log(x)
+
+// // //this sis comment and it will not be executed
+
+// //  x="siddharth"
+// // // let x=10
+
+// // console.log(x)
+
+// // let A1="Siddharth"
+// // let A2="vishen"
+// // A1="vishen"
+// // console.log(A1)
+
+// // const _Name="Siddharth hidden"
+// // //_Name is a hidden variable
+
+// // console.log(_Name)
+
+// // let age=12,name="naamsiddharth"
+// // console.log(name)
+
+
+// // let siddate=new Date()
+// // console.log(siddate.setFullYear(2024))
+
+
+// // let rand=Math.random()
+// // console.log(rand)
+
+// // // let _1="Siddharth"
+// // // console.log(_1)
+
+// // let Name="siddharth",Age=23,Prof="Web Developer"
+
+// // console.log(Name)
+// // console.log(Age)
+// // console.log(Prof)
+
+// // let numb="5"
+// // console.log(numb+5+9+10)
+
+// let x
+
+// x=10
 // console.log(x)
 
-//this sis comment and it will not be executed
+// z=1000
+// var z
 
- x="siddharth"
-// let x=10
 
+// console.log(z)
+
+// var ab=100
+// function sid() {
+//     var ab=1000
+//     console.log(x)
+// }
+
+
+// console.log(ab)
+
+// yash=10
+// let yashv
+// console.log(yashv)
+
+// var yash
+// yashv=20
+
+// var sidv={
+//     name:"Siddharth",
+//     profession:"Web Dev",
+//     time:function(){
+//         let x=34
+//         console.log(x)
+//     }
+// }
+
+
+// sidv.time()
+
+// sidv.cooks="Halwa"
+// console.log(sidv)
+
+// let demo=document.getElementById("demo")
+
+// let time=document.getElementById("time")
+// console.log(demo)
+
+// function disptime() {
+//     demo.innerHTML=Date()
+// }
+
+// time.addEventListener("click",disptime)
+
+
+// let sid="Siddharth"
+// console.log(sid[0])
+
+// console.log(sid.charAt(2))
+
+// console.log(sid.at(-2))
+
+// let as="Siddharth"
+// let food=prompt("your fav food is ")
+
+// console.log(`My name is ${as} and my fav food is ${food}`)
+
+let y=new Number(23)
+console.log(y)
+
+let x=new Number(45)
 console.log(x)
 
-let A1="Siddharth"
-let A2="vishen"
-A1="vishen"
-console.log(A1)
 
-const _Name="Siddharth hidden"
-//_Name is a hidden variable
-
-console.log(_Name)
-
-let age=12,name="naamsiddharth"
-console.log(name)
-
-
-let siddate=new Date()
-console.log(siddate.setFullYear(2024))
-
-
-let rand=Math.random()
-console.log(rand)
-
-// let _1="Siddharth"
-// console.log(_1)
-
-let Name="siddharth",Age=23,Prof="Web Developer"
-
-console.log(Name)
-console.log(Age)
-console.log(Prof)
-
-let numb="5"
-console.log(numb+5+9+10)
-
-let x
-
-x=10
-console.log(x)
+let ab=10.345
+console.log(ab.toExponential(2))

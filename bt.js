d1a84aa5dab6e454ddd3806612560060c41dfd44
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2024-03-12 18:35:14 +0530

    codes added

---
+++ bt.js
@@ -0,0 +1,7 @@
+
+try{
+    alddert("Hi I am siddharth")
+}
+catch(err){
+    console.log(err.msg)
+}
--- shoppingcart.js
+++ shoppingcart.js
@@ -1,206 +1,215 @@
-// // let sid=document.getElementsByTagName('h1')[0]
+// // // let sid=document.getElementsByTagName('h1')[0]
 
-// // function Sid() {
-// //     // sid.innerHTML="I am clicked and now I am Sid Vishen"
-// //     // document.write(5+5)
-// //     // window.alert("Siddharth")
-// //     document.getElementsByTagName('button')[0].style.display="none"
+// // // function Sid() {
+// // //     // sid.innerHTML="I am clicked and now I am Sid Vishen"
+// // //     // document.write(5+5)
+// // //     // window.alert("Siddharth")
+// // //     document.getElementsByTagName('button')[0].style.display="none"
 
-// //     window.print()
+// // //     window.print()
 
-// // }
-
-
-// // sid.addEventListener('click',Sid)
+// // // }
 
 
-// // // let x,y,z
+// // // sid.addEventListener('click',Sid)
 
-// // // x="sodd"
+
+// // // // let x,y,z
+
+// // // // x="sodd"
+
+// // // // console.log(x)
+
+// // // //this sis comment and it will not be executed
+
+// // //  x="siddharth"
+// // // // let x=10
 
 // // // console.log(x)
 
-// // //this sis comment and it will not be executed
+// // // let A1="Siddharth"
+// // // let A2="vishen"
+// // // A1="vishen"
+// // // console.log(A1)
 
-// //  x="siddharth"
-// // // let x=10
+// // // const _Name="Siddharth hidden"
+// // // //_Name is a hidden variable
 
+// // // console.log(_Name)
+
+// // // let age=12,name="naamsiddharth"
+// // // console.log(name)
+
+
+// // // let siddate=new Date()
+// // // console.log(siddate.setFullYear(2024))
+
+
+// // // let rand=Math.random()
+// // // console.log(rand)
+
+// // // // let _1="Siddharth"
+// // // // console.log(_1)
+
+// // // let Name="siddharth",Age=23,Prof="Web Developer"
+
+// // // console.log(Name)
+// // // console.log(Age)
+// // // console.log(Prof)
+
+// // // let numb="5"
+// // // console.log(numb+5+9+10)
+
+// // let x
+
+// // x=10
 // // console.log(x)
 
-// // let A1="Siddharth"
-// // let A2="vishen"
-// // A1="vishen"
-// // console.log(A1)
-
-// // const _Name="Siddharth hidden"
-// // //_Name is a hidden variable
-
-// // console.log(_Name)
-
-// // let age=12,name="naamsiddharth"
-// // console.log(name)
+// // z=1000
+// // var z
 
 
-// // let siddate=new Date()
-// // console.log(siddate.setFullYear(2024))
+// // console.log(z)
+
+// // var ab=100
+// // function sid() {
+// //     var ab=1000
+// //     console.log(x)
+// // }
 
 
-// // let rand=Math.random()
-// // console.log(rand)
+// // console.log(ab)
 
-// // // let _1="Siddharth"
-// // // console.log(_1)
+// // yash=10
+// // let yashv
+// // console.log(yashv)
 
-// // let Name="siddharth",Age=23,Prof="Web Developer"
+// // var yash
+// // yashv=20
 
-// // console.log(Name)
-// // console.log(Age)
-// // console.log(Prof)
+// // var sidv={
+// //     name:"Siddharth",
+// //     profession:"Web Dev",
+// //     time:function(){
+// //         let x=34
+// //         console.log(x)
+// //     }
+// // }
 
-// // let numb="5"
-// // console.log(numb+5+9+10)
 
-// let x
+// // sidv.time()
 
-// x=10
+// // sidv.cooks="Halwa"
+// // console.log(sidv)
+
+// // let demo=document.getElementById("demo")
+
+// // let time=document.getElementById("time")
+// // console.log(demo)
+
+// // function disptime() {
+// //     demo.innerHTML=Date()
+// // }
+
+// // time.addEventListener("click",disptime)
+
+
+// // let sid="Siddharth"
+// // console.log(sid[0])
+
+// // console.log(sid.charAt(2))
+
+// // console.log(sid.at(-2))
+
+// // let as="Siddharth"
+// // let food=prompt("your fav food is ")
+
+// // console.log(`My name is ${as} and my fav food is ${food}`)
+
+// let y=new Number(23)
+// console.log(y)
+
+// let x=new Number(45)
 // console.log(x)
 
-// z=1000
-// var z
+
+// let ab=10.345
+// console.log(ab.toExponential(2))
 
 
-// console.log(z)
+// // let sid=[1,2,3,4,5,6,7,8,9,10]
 
-// var ab=100
-// function sid() {
-//     var ab=1000
-//     console.log(x)
-// }
+// // console.log(sid.slice(4,-4))
 
+// // let vis="      Sid              "
+// // let fir="vis"
+// // console.log(vis.concat('',fir))
 
-// console.log(ab)
+// // console.log(vis.repeat(2))
 
-// yash=10
-// let yashv
-// console.log(yashv)
+// // let as="Siddharth"
+// // let food=promt("your fav food is ")
 
-// var yash
-// yashv=20
+// // console.log(`My name is ${as} and my fav food is ${food}`)
 
-// var sidv={
+// // let y=new Number(123)
+// // console.log(y.EPSILON)
+
+// let sid=["mango","potato","Grapes"]
+// let vishen=["papad","aachar","kapoor",["mango","potato","Grapes"]]
+// let shahi=["Riyon","taramandal","siktour"]
+// const newsid=sid.concat("Visen")
+// console.log(vishen.flat())
+// // console.log(sid.pop())
+// // console.log(sid.push("Sidd"))
+// // console.log(sid)
+// // console.log(sid.at(-3))
+// // console.log(sid.toString())
+// // console.log(sid.length-1)
+
+// // console.log(Array.isArray(sid))
+
+// // let sid=[1,2,3,4,5,6,7,8,100]
+// const price=[23,12,34,22,65,120]
+// const minp=Math.max(23,12,34,22,65,120)
+// console.log(`Max Funct-${minp}`)
+// console.log(Math.log(10))
+// const sid=Math.round(4.56)
+// const vss=Math.ceil(4.56)
+// const sss=Math.floor(4.56)
+// const dss=Math.trunc(4.56)
+// const tss=Math.sign(0)
+// console.log(`tss-sign ${tss}`)
+// console.log(`dss-trunc ${dss}`)
+// console.log(`sss-floor ${sss}`)
+// console.log(`vss-ceil ${vss}`)
+// console.log(`sid-round ${sid}`)
+
+// //To use math object we do not have to create object
+
+// const sid={
 //     name:"Siddharth",
-//     profession:"Web Dev",
-//     time:function(){
-//         let x=34
-//         console.log(x)
-//     }
+//     education:"B.Tech",
+//     profession:"Web Developer",
+//     hobby:"Reading books"
 // }
 
+// console.log(sid.name)
 
-// sidv.time()
-
-// sidv.cooks="Halwa"
-// console.log(sidv)
-
-// let demo=document.getElementById("demo")
-
-// let time=document.getElementById("time")
-// console.log(demo)
-
-// function disptime() {
-//     demo.innerHTML=Date()
+// for(i in sid){
+//     console.log(` object-${i}:${sid[i]}`)
 // }
 
-// time.addEventListener("click",disptime)
+// console.log(Object.keys(sid))
+// const val=Object.values(sid)
+// console.log(typeof(val))
+// console.log(Array.isArray(val))
 
-
-// let sid="Siddharth"
-// console.log(sid[0])
-
-// console.log(sid.charAt(2))
-
-// console.log(sid.at(-2))
-
-// let as="Siddharth"
-// let food=prompt("your fav food is ")
-
-// console.log(`My name is ${as} and my fav food is ${food}`)
-
-let y=new Number(23)
-console.log(y)
-
-let x=new Number(45)
-console.log(x)
-
-
-let ab=10.345
-console.log(ab.toExponential(2))
-
-
-// let sid=[1,2,3,4,5,6,7,8,9,10]
-
-// console.log(sid.slice(4,-4))
-
-// let vis="      Sid              "
-// let fir="vis"
-// console.log(vis.concat('',fir))
-
-// console.log(vis.repeat(2))
-
-// let as="Siddharth"
-// let food=promt("your fav food is ")
-
-// console.log(`My name is ${as} and my fav food is ${food}`)
-
-// let y=new Number(123)
-// console.log(y.EPSILON)
-
-let sid=["mango","potato","Grapes"]
-let vishen=["papad","aachar","kapoor",["mango","potato","Grapes"]]
-let shahi=["Riyon","taramandal","siktour"]
-const newsid=sid.concat("Visen")
-console.log(vishen.flat())
-// console.log(sid.pop())
-// console.log(sid.push("Sidd"))
-// console.log(sid)
-// console.log(sid.at(-3))
-// console.log(sid.toString())
-// console.log(sid.length-1)
-
-// console.log(Array.isArray(sid))
-
-// let sid=[1,2,3,4,5,6,7,8,100]
-const price=[23,12,34,22,65,120]
-const minp=Math.max(23,12,34,22,65,120)
-console.log(`Max Funct-${minp}`)
-console.log(Math.log(10))
-const sid=Math.round(4.56)
-const vss=Math.ceil(4.56)
-const sss=Math.floor(4.56)
-const dss=Math.trunc(4.56)
-const tss=Math.sign(0)
-console.log(`tss-sign ${tss}`)
-console.log(`dss-trunc ${dss}`)
-console.log(`sss-floor ${sss}`)
-console.log(`vss-ceil ${vss}`)
-console.log(`sid-round ${sid}`)
-
-//To use math object we do not have to create object
-
-const sid={
-    name:"Siddharth",
-    education:"B.Tech",
-    profession:"Web Developer",
-    hobby:"Reading books"
-}
-
-console.log(sid.name)
-
-for(i in sid){
-    console.log(` object-${i}:${sid[i]}`)
-}
-
-console.log(Object.keys(sid))
-const val=Object.values(sid)
-console.log(typeof(val))
-console.log(Array.isArray(val))
+// try{
+//     alddert("Hi I am siddharth")
+// }
+// catch(e){
+//     console.log(sid.name)
+//     console.log(sid.msg)
+// }
+// throw new Error {"Its related to alert"}

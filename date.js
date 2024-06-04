37ece5c373eeb2e624eac60ad44b9d8054aecad7
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2024-06-05 04:55:46 +0530

    codes added

--- date.js
+++ date.js
@@ -1,1 +1,8 @@
-alert("connected")
+// alert("connected")
+
+const sid=document.querySelector('h1')
+setTimeout(function(){
+    sid.textContent="I am siddharth"
+},5000)
+alert("I am after settimeout")
+
